e13123a85a2cc7fd941f8efe22394b9eebd26306
Rostyslav Kravchenko <rostyslav.kravchenko@tui.co.uk>
2025-03-11 15:44:01 +0200

    fix

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,6 @@
-import './App.css';
-
 function App() {
   return (
-    <div className="container">
-      <header className="header">
-        <h1>Interview Kit</h1>
-      </header>
-      <section className="main">
-        <input type="text" onChange={() => console.log(1)} placeholder='text' />
-      </section>
-    </div>
+    <input type="text" placeholder="text" />
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
+import './components/App/App.css';
 import { MainProvider } from './context/MainProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <MainProvider>
-    <App />
+    <div className="container">
+      <header className="header">
+        <h1>Interview Kit</h1>
+      </header>
+      <div className="main">
+        <App />
+      </div>
+    </div>
   </MainProvider>
 );
 
